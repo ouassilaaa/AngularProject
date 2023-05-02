886d577e477323ba7a64488b0477d785f8f0425a
ouassilaaa <ouassidu3@hotmail.fr>
2023-05-02 14:20:25 +0200

    composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import  {SuperComponent} from './super/super.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { LesAmisComponent } from './les-amis/les-amis.component';
+import { UnAmiComponent } from './un-ami/un-ami.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuperComponent,
+    LesAmisComponent,
+    UnAmiComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/super/super.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-super',
+  templateUrl: './super.component.html',
+  styleUrls: ['./super.component.css']
+})
+
+export class SuperComponent {
+  
+}
+
+
+
