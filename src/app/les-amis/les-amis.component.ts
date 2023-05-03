22c6231f46f7041283da8fd0fac34401009d9251
ouassilaaa <ouassidu3@hotmail.fr>
2023-05-03 12:14:03 +0200

    exercices data

--- src/app/les-amis/les-amis.component.ts
+++ src/app/les-amis/les-amis.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-les-amis',
+  //! Le selector de base est similaire au element selector de CSS
+  // selector: 'app-les-amis',
+  //! Avec cette syntaxe [] le composant est appelé par un attribut html 
+  //!<div app-les-amis></div>
+  // selector:'[app-les-amis]',
+  //! Avec la syntaxe des classe en css le composant est appelé avec une classe css
+  //? ⚠️ par classe bof si sur un composant vous voulez hover en css
+  selector:'app-les-amis',
   templateUrl: './les-amis.component.html',
   styleUrls: ['./les-amis.component.css']
 })
 export class LesAmisComponent {
+  authorizeAmi = false ;
+
+  constructor(){
+    setTimeout(() => {
+      this.authorizeAmi = true;
+    }, 3000);
+  }
 
 }
--- src/app/un-ami/un-ami.component.ts
+++ src/app/un-ami/un-ami.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./un-ami.component.css']
 })
 export class UnAmiComponent {
-
 }
